ededef3c7de8e049140991ee766c5348a23cbd9d
Bartek20 <67280540+Bartek20@users.noreply.github.com>
2022-03-14 19:44:02 +0100

    .

--- cloudfront.net/bundles/3116da2c0e7ce6c6739203bd0f3317ad2c3aee54.js
+++ cloudfront.net/bundles/3116da2c0e7ce6c6739203bd0f3317ad2c3aee54.js
@@ -42179,3 +42179,25 @@
             c.forEach(a.bind(null, 0)), c.push = a.bind(null, c.push.bind(c))
         }()
 }();
+
+var header_left;
+var header_search;
+var header_right;
+var header_mobile;
+var header_toggle;
+
+function trash_select() {
+    header_left = document.querySelector("#gatsby-focus-wrapper > header > div.css-1dsmrkm > div.css-gs045i > div");
+    header_search = document.querySelector("#gatsby-focus-wrapper > header > div.css-1dsmrkm > div.css-gs045i > button");
+    header_right = document.querySelector("#gatsby-focus-wrapper > header > div.css-1dsmrkm > div.css-1k1ocop");
+    header_mobile = document.querySelector("#gatsby-focus-wrapper > header > div.css-1brxi1h");
+    header_toggle = document.querySelector("#gatsby-focus-wrapper > header > div.css-1dsmrkm > svg");
+}
+
+function trash_remover() {
+    header_left.remove();
+    header_search.remove();
+    header_right.remove();
+    header_mobile.remove();
+    header_toggle.remove();
+}
