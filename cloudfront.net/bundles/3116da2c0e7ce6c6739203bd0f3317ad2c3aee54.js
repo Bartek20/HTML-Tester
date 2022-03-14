b8c32b3703c1e31b2433cb4f0f1c4bdd6dcd23b9
Bartek20 <67280540+Bartek20@users.noreply.github.com>
2022-03-14 21:42:09 +0100

    .

--- cloudfront.net/bundles/3116da2c0e7ce6c6739203bd0f3317ad2c3aee54.js
+++ cloudfront.net/bundles/3116da2c0e7ce6c6739203bd0f3317ad2c3aee54.js
@@ -42185,16 +42185,40 @@ var header_search;
 var header_right;
 var header_mobile;
 var header_toggle;
+var retry = 0;
 
-async function select()
+function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms));}
+
+function select()
 {
-    while (header_left == null || header_search == null || header_right == null || header_mobile == null || header_toggle == null)
+    try
     {
         header_left = document.querySelector("#gatsby-focus-wrapper > header > div.css-1dsmrkm > div.css-gs045i > div");
         header_search = document.querySelector("#gatsby-focus-wrapper > header > div.css-1dsmrkm > div.css-gs045i > button");
         header_right = document.querySelector("#gatsby-focus-wrapper > header > div.css-1dsmrkm > div.css-1k1ocop");
         header_mobile = document.querySelector("#gatsby-focus-wrapper > header > div.css-1brxi1h");
         header_toggle = document.querySelector("#gatsby-focus-wrapper > header > div.css-1dsmrkm > svg");
+        if ((header_left == null) || (header_search == null) || (header_right == null) || (header_mobile == null) || (header_toggle == null))
+        {
+            throw "NULL Err";
+        }
+    }
+    catch (err)
+    {
+        if (err == "NULL Err")
+        {
+            sleep(100);
+            retry++;
+            if (retry <= 5)
+            {
+                console.log("Próba", retry);
+                select();
+            }
+            else
+            {
+                console.log("Niepowodzenie...");
+            }
+        }
     }
 }
 
